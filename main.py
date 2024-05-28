44b68b3fb64a4c5fc524b78d93907aed6f7e098a
oeduardosg <eduardosg.my@gmail.com>
2024-05-28 18:01:21 -0300

    Create the selection menu prototype

--- main.py
+++ main.py
@@ -11,10 +11,13 @@ LENGHT = 768
 window = pygame.display.set_mode((WIDTH, LENGHT))
 pygame.display.set_caption("Don't Starve by Them")
 
-#Adjusting the background size
-background_menu = pygame.image.load("images/menu_backgrounds/background_1.webp").convert()
+#Adjusting the menu background size
+background_menu = pygame.image.load("images/backgrounds/background_1.webp").convert()
 h = background_menu.get_width()
 background_menu = pygame.transform.scale_by(background_menu, WIDTH/h)
+
+#Adjusting the selection background
+background_selection = pygame.image.load("images/backgrounds/background_1.webp")    #ADICIONAR!!!!!!!!!!!!!!!!!!!!!
 
 #Creating the main groups
 characters = pygame.sprite.Group()
@@ -32,5 +35,9 @@ run = True
 while run:
 
     run = menu(window, background_menu, clock, fps)
+    if run == 1:
+        run = selection(characters, window, background_selection, clock, fps)
+        #if run:
+            #run = level(characters, enemies, window, background_level, clock, fps)
 
 pygame.quit()
--- menu.py
+++ menu.py
@@ -1,4 +1,5 @@
 import pygame
+from entities import *
 
 def menu(window, background_menu, clock, fps):
 
@@ -16,6 +17,39 @@ def menu(window, background_menu, clock, fps):
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                run = False
+                return False
+            if event.type == pygame.KEYDOWN:
+                if pygame.K_a:
+                    return 1
+                
+
+def selection(characters, window, background_selection, clock, fps):
+
+    frame_image = pygame.image.load("images/selection_frame.png").convert()
+    #h = frame_image.get_width()
+    #frame_image = pygame.transform.scale_by(frame_image, WIDTH/h)
+    x = 50
+    y = 50
+
+    while len(characters.sprites()) < 3:
+
+        window.blit(background_selection, (0,0))
+        window.blit(frame_image, (x, y))
+
+        clock.tick(fps)
+        pygame.display.flip()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return False
+            if event.type == pygame.KEYDOWN:
+                if event.type == pygame.K_UP:
+                    y -= 10
+                if event.type == pygame.K_DOWN:
+                    y += 10
+                if event.type == pygame.K_RIGHT:
+                    x += 10
+                if event.type == pygame.K_LEFT:
+                    x -= 10
 
     return False
